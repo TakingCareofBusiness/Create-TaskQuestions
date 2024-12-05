2c68c9d8822c6a769bd165489c9b4bd76db1b9c7
TakingCareofBusiness <superspongysquid@yahoo.com>
2024-12-05 12:31:00 -0500

    Bulbjak

---
+++ answers.d
@@ -0,0 +1,11 @@
+Angelina Stuff
+
+1
+
+2
+
+Butter Stuff
+
+1 The users input is category, and category is used to retrieve the date from the api of quotes that fit into that category. The code then prints cards that fit the category that was inputted, and the inputted category is placed at the bottom of each card.
+
+2
