bf8b81d921ce15f9459b7c0d95ec4651f6e98fd7
TakingCareofBusiness <superspongysquid@yahoo.com>
2024-12-10 12:29:39 -0500

    Time to get a watch

--- answers.d
+++ answers.d
@@ -1,8 +1,8 @@
 Angelina Stuff
 
-1
+1 It takes a text file in the function insertText, then finds the text in the file and prints them out as <p></p> in the div "results".
 
-2
+2 The first iteration in the code is a forEach command, which runs for every item in the text file that you inputted. Then, for each item in the inputted content, the code puts the text from each item onto the screen and website.    insertText(randomfile, "file") This call takes the file, finds each item in the file, and adds each item to the website, and since you added a file, each item is text.     insertText(randomthing, "photo") This call takes whatever you inputted, which in this case isn't a file, and adds the text content of each item within whatever you inputted, so text is still added to the website, even if what you inputted didn't neatly package the text in a file.
 
 Butter Stuff
 
